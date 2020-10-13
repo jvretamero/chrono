9893fbe010022f3f68796d7f80ed2d13dce23ad3
João Vitor Retamero <joaovretamero@gmail.com>
2020-10-13 18:22:04 -0300

    Extração da lógica do calendário

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     var formatoData = new Intl.DateTimeFormat("pt-BR");
 
     var elRaiz = document.documentElement;
-    var diasMes = document.getElementById("dias-mes");
     var dataSelecionada = document.getElementById("data-selecionada");
 
     var hoje = new Date();
@@ -10,78 +9,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var diaSelecionado = hoje;
 
-    var navegacaoMes = window.navegacaoMes(function () {
-        atualizarDias();
+    var calendario = window.calendario(function (dia) {
+        diaSelecionado = dia;
+        atualizarAgendamentos();
+    });
+    calendario.iniciar(hoje);
+
+    var navegacaoMes = window.navegacaoMes(function (mes, ano) {
+        calendario.atualizarMes(mes, ano);
     });
     navegacaoMes.iniciar(hoje);
-
-    function definirDia(el) {
-        if (el.classList.contains("ativo"))
-            return;
-
-        var diaAtivo = document.querySelector(".table-mes td.ativo");
-        if (diaAtivo)
-            diaAtivo.classList.remove("ativo");
-
-        el.classList.add("ativo");
-
-        diaSelecionado = new Date(el.dataset.data);
-
-        atualizarAgendamentos();
-    }
-
-    function criarTrDia(ano, mes, dia) {
-        var td = document.createElement("td");
-
-        if (ano != null && mes != null && dia != null) {
-            td.innerText = dia;
-            td.dataset.dia = dia;
-            td.dataset.data = new Date(ano, mes, dia);
-            td.classList.add("com-dia");
-            td.addEventListener("click", function (e) {
-                definirDia(this,);
-            });
-        }
-
-        return td;
-    }
-
-    function atualizarDias() {
-        var mesAtual = navegacaoMes.obterMes();
-        var anoAtual = navegacaoMes.obterAno();
-
-        diasMes.innerHTML = "";
-
-        var totalDias = new Date(anoAtual, mesAtual + 1, 0).getDate();
-        var dia = 1;
-        var primeiroDia = new Date(anoAtual, mesAtual);
-        var didSemanaInicial = primeiroDia.getDay();
-
-        for (var semana = 0; semana < 6; semana++) {
-            var semanaTr = document.createElement("tr");
-
-            for (var diaSemana = 0; diaSemana < 7; diaSemana++) {
-                var naoComecouMes = semana == 0 && diaSemana < didSemanaInicial;
-                var terminouMes = dia > totalDias;
-
-                if (naoComecouMes || terminouMes) {
-                    semanaTr.appendChild(criarTrDia());
-                } else {
-                    semanaTr.appendChild(criarTrDia(anoAtual, mesAtual, dia));
-                    dia++;
-                }
-            }
-
-            diasMes.appendChild(semanaTr);
-        }
-
-        if (diaSelecionado.getMonth() == mesAtual && diaSelecionado.getFullYear() == anoAtual) {
-            var tdDia = document.querySelector(`.table-mes td[data-dia="${diaSelecionado.getDate()}"]`);
-            if (tdDia) definirDia(tdDia);
-        }
-    };
-
-    atualizarDias();
 
     var modalAgendamento = document.getElementById("modal-agendamento");
 
