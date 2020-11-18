5ead3ace39ddb405e9c842090a04aaf9dc96bb22
João Vitor Retamero <joaovretamero@gmail.com>
2020-11-18 06:41:44 -0300

    Correção modelo da lista de agendamento

--- src/componentes/ItemAgendamento.vue
+++ src/componentes/ItemAgendamento.vue
@@ -1,12 +1,12 @@
 <template>
     <li class="agendamento">
         <div class="agendamento-horario">
-            <span class="horario-inicio">{{ agendamento.hora.incial }}</span>
+            <span class="horario-inicio">{{ agendamento.hora.inicial }}</span>
             <span class="horario-termino">{{ agendamento.hora.final }}</span>
         </div>
         <div class="agendamento-dados">
             <span class="categoria">{{ agendamento.categoria }}</span>
-            <span class="titulo">{{ agendamento.titulo }}</span>
+            <span class="titulo">{{ agendamento.assunto }}</span>
         </div>
     </li>
 </template>
