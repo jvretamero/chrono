5e6ac2b8df03e021becb72a2b6ff878200e2bf54
João Vitor Retamero <joaovretamero@gmail.com>
2020-11-19 06:26:41 -0300

    Lidando com agendamentos em memória

--- src/servicos/persistencia.js
+++ src/servicos/persistencia.js
@@ -2,22 +2,36 @@ import { gerarId } from "./utils";
 
 const CHAVE_AGENDAMENTOS = "agendamentos";
 const storage = window.sessionStorage;
+let listaAgendamento = null;
 
 const persistirAgendamentos = function (agendamentos) {
     storage.setItem(CHAVE_AGENDAMENTOS, JSON.stringify(agendamentos));
 };
 
 export const obterAgendamentos = function () {
-    //TODO tratar data que é armazenada como string
-    var agendamentosStr = storage.getItem(CHAVE_AGENDAMENTOS);
-    return agendamentosStr ? JSON.parse(agendamentosStr) : [];
+    if (listaAgendamento == null) {
+        const agendamentosStr = storage.getItem(CHAVE_AGENDAMENTOS);
+        const agendamentos = agendamentosStr ? JSON.parse(agendamentosStr) : [];
+
+        listaAgendamento = {};
+
+        // Ajuste necessário pois as datas foram salvas como JSON
+        // ou seja, transformadas em string
+        agendamentos.forEach(function (agendamento) {
+            agendamento.data = new Date(agendamento.data);
+
+            listaAgendamento[agendamento.id] = agendamento;
+        });
+    }
+
+    return listaAgendamento;
 };
 
 export const persistirAgendamento = function (agendamento) {
     agendamento.id = gerarId();
 
-    var agendamentos = obterAgendamentos();
-    agendamentos.push(agendamento);
+    const agendamentos = obterAgendamentos();
+    agendamentos[agendamento.id] = agendamento;
 
     persistirAgendamentos(agendamentos);
 };
